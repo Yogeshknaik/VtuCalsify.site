3589d7ec333cfe1bea5a9aeb99953e5a475da830
YOGESH K N <107015868+Yogeshknaik@users.noreply.github.com>
2023-08-22 23:19:43 +0530

    Add files via upload

---
+++ VtuCalcify.site/animals.js
@@ -0,0 +1,21 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+
+    if (x === "21MAT31") {
+        window.open("https://drive.google.com/drive/folders/1Cd5aF-EcbSslVZXhd6xEB9kvA3rZr7AK?usp=share_link");
+    }
+
+    if (x === "21CS33") {
+        window.open("https://drive.google.com/drive/folders/1xC69E-BQhOYd8ccV7LjOlTXCYALJcSsJ?usp=share_link");
+    }
+    if (x === "21CS34") {
+        window.open("https://drive.google.com/drive/folders/1iNexY_69Dk762pL2QPjbJc4E05CYRdrP?usp=share_link");
+    }
+    if (x === "21CS32") {
+        window.open("https://drive.google.com/drive/folders/1gw4Xh3H235ez6RrHguuoQmrUwCbIFBAp?usp=share_link");
+    }
+    if (x === "21CSL35") {
+        window.open("https://drive.google.com/drive/folders/1qtqx-41p6aPKh_7Fj4-Z80zWRRyN7RFF?usp=share_link");
+    }
+
+}
